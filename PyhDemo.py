bb6bc35bf4dbae421d1acbbef5c2a8cf4e3bd573
manjunath-grl <manjunath.grl@outlook.com>
2022-11-17 22:47:39 +0530

    Added some more Hooks

--- PyhDemo.py
+++ PyhDemo.py
@@ -1,8 +1,8 @@
 import json
 
-with open("package.json", "r") as f:
-    data = json.load(f)
+with open("package.json", encoding="utf-8") as f:
+    data_ = json.loads(f.read())
 
-print(data)
-print(data["name"])
+print(data_)
+print(data_["name"])
 print("done")

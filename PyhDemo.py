b49a2e7464eab8046d8bf708cc4bf6580897dd1f
manjunath-grl <manjunath.grl@outlook.com>
2022-11-17 21:31:18 +0530

    Py

--- PyhDemo.py
+++ PyhDemo.py
@@ -3,7 +3,6 @@ import json
 with open("package.json", "r") as f:
     data = json.load(f)
 
-# Output: {'name': 'Bob', 'languages': ['English', 'French']}
 print(data)
 print(data["name"])
 print("done")

e27db0cfe2700baaf3a6426048ea252119b8352e
manjunath-grl <manjunath.grl@outlook.com>
2022-11-18 13:03:46 +0530

    No verify

--- PyhDemo.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open("package.json", encoding="utf-8") as f:
-    data_ = json.loads(f.read())
-
-print(data_)
-print(data_["name"])
-print("done")
---
+++ pyh_demo.py
@@ -0,0 +1,37 @@
+import json
+
+with open("package.json", encoding="utf-8") as f:
+    read_data = json.loads(f.read())
+
+print(read_data)
+print(read_data["name"])
+print("done")
+
+
+arr = [2, 3, 4, 5]
+
+for i in arr:
+    print(i)
+
+
+# function
+def multiplier(fst_ele, sec_ele):
+    """Takes in two numbers, returns their product."""
+    return fst_ele * sec_ele
+
+
+print(multiplier(20, 40))
+
+
+# Python String Operations
+def string_reverse(strng):
+
+    reverse_str = ""
+    j = len(strng)
+    while j > 0:
+        reverse_str += strng[j - 1]
+        j = j - 1
+    return reverse_str
+
+
+print(string_reverse("GRL"))
